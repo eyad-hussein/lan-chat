40e8bcf7d5ad7ee9e7bc37c43ea03a33058eb586
eyad-hussein <eyad.hussein@ejust.edu.eg>
2024-05-05 18:36:26 +0300

    fix broadcast issue

--- server.js
+++ server.js
@@ -4,19 +4,24 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const PORT = 3000;
+const PORT = 5000;
 const LAN_IP = "192.168.106.175";
+
+let chatHistory = [];
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/app/views/index.html");
 });
 
 io.on("connection", (socket) => {
+  socket.emit("chat history", chatHistory);
+
   socket.on("chat message", (msg) => {
+    chatHistory.push(msg);
     io.emit("chat message", msg);
   });
 });
 
-server.listen(PORT, LAN_IP, () => {
+server.listen(PORT, () => {
   console.log("working");
 });
