39ca9d7d8d9725d745d85102f03310db6b9638cc
omar-abdelgawad <omar.abdelgawad@ejust.edu.eg>
2024-05-07 03:06:58 +0300

    Altered project structure to public/
    added nicknames
    
    Changed style

---
+++ public/scripts/client.js
@@ -0,0 +1,51 @@
+const socket = io();
+
+const messages = document.getElementById("messages");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const ul = document.querySelector("ul");
+const ip = window.location.hostname;
+const nickname = prompt("What is your nickname?") || ip;
+socket.emit("nickname", nickname);
+
+input.focus();
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (input.value) {
+        const message_to_send = { 'msg': input.value }
+        socket.emit("chat message", message_to_send);
+        input.value = "";
+    }
+});
+
+socket.on("chat message", function (received_message) {
+    appendMessage(received_message);
+    ul.scrollTop = ul.scrollHeight;
+    window.scrollTo(0, document.body.scrollHeight);
+
+});
+
+socket.on("chat history", function (history) {
+    history.forEach(function (received_message) {
+        received_message = received_message[0]; // so weird
+        appendMessage(received_message);
+        ul.scrollTop = ul.scrollHeight;
+        window.scrollTo(0, document.body.scrollHeight);
+    });
+});
+
+// define a function that takes as input the received_message and appends the li to messages
+function appendMessage(received_message) {
+    const item = document.createElement("li");
+    item.innerHTML = `
+    <p>${received_message['msg']}</p>
+    <div>${received_message['nickname']}</div>
+    `;
+    if (received_message['nickname'] === nickname) {
+        item.setAttribute("class", "me");
+    } else {
+        item.setAttribute("class", "others");
+    }
+    messages.appendChild(item);
+}
--- server.js
+++ server.js
@@ -5,23 +5,29 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const PORT = 5000;
-const LAN_IP = "192.168.106.175";
+// const LAN_IP = "192.168.106.175";
 
 let chatHistory = [];
+let user_ip_to_nickname = {};
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/app/views/index.html");
-});
+app.use(express.static("./public"));
 
 io.on("connection", (socket) => {
+  const user_ip = socket.handshake.address.split(":").pop();
+  console.log(`a user connected on ${user_ip}`);
   socket.emit("chat history", chatHistory);
 
-  socket.on("chat message", (msg) => {
-    chatHistory.push(msg);
-    io.emit("chat message", msg);
+  socket.on("chat message", (received_message) => {
+    received_message = { ...received_message, nickname: user_ip_to_nickname[user_ip] };
+    chatHistory.push([received_message]);
+    io.emit("chat message", received_message);
+  });
+
+  socket.on('nickname', (nickname) => {
+    user_ip_to_nickname[user_ip] = nickname;
   });
 });
 
 server.listen(PORT, () => {
-  console.log("working");
+  console.log(`working on port ${PORT}`);
 });
